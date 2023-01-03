8a3be7fb519ab812c5e55df4ae7ef99f7aacf00f
Monduli <dcglendon@gmail.com>
2023-01-03 17:22:25 -0500

    Properly committing using the GitHub CLI as it wasn't tracking my other commits

--- app.cpp
+++ app.cpp
@@ -8,24 +8,33 @@ int main() {
     // Prints I am learning C++ after the newline from the previous line.
     cout << "I am learning C++. \n";
 
+    // int are numbers as they are in other languages
     int num;
     num = 15;
 
+    // const means it won't change
     const int myNum = 15;
     cout << myNum << "\n";
 
+    // double are ^-1 decimal numbers
     double myFloat = 5.99;
+    // char is single character
     char myLetter = 'D';
+    // string is multiple characters
     string myText = "Hello";
+    // boolean is true/false like other languages
     bool myBoolean = true;
 
+    // using << multiple times allows for subbing in variables
     cout << "I am " << myNum << " years old. \n";
 
+    // adding variables. also adding \n to the end to escape lines despite string/int mix
     int x = 5, y = 6, z = 50;
     cout << x + y + z << "\n";
     x = y = z = 50;
     cout << x + y + z << "\n";
 
+    // cin is "c input" and cout is "c output" (might stand for something else)
     string input;
     cout << "Type something. \n";
     cin >> input;
